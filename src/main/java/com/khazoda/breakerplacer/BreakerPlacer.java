56d027347a916a8bdcdf24d77268cbf7fd0b5b2d
Khazoda <khazoda@gmail.com>
2024-07-26 23:05:04 +0100

    lang, itemgroup reg

--- src/main/java/com/khazoda/breakerplacer/BreakerPlacer.java
+++ src/main/java/com/khazoda/breakerplacer/BreakerPlacer.java
@@ -2,6 +2,9 @@ package com.khazoda.breakerplacer;
 
 import com.khazoda.breakerplacer.registry.RBlock;
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
+import net.minecraft.item.ItemGroups;
+import net.minecraft.item.Items;
 
 public class BreakerPlacer implements ModInitializer {
 	public static int loadedRegistries = 0;
@@ -12,7 +15,9 @@ public class BreakerPlacer implements ModInitializer {
 
 		RBlock.init();
 
+		ItemGroupEvents.modifyEntriesEvent(ItemGroups.REDSTONE).register(content -> content.addAfter(Items.CRAFTER, RBlock.PLACER_BLOCK));
+		ItemGroupEvents.modifyEntriesEvent(ItemGroups.REDSTONE).register(content -> content.addAfter(Items.CRAFTER, RBlock.BREAKER_BLOCK));
 
-		Constants.LOG.info("[BB&BP] {}/5 Blocks placed!",loadedRegistries);
+		Constants.LOG.info("[BB&BP] {}/1 Blocks placed!",loadedRegistries);
 	}
 }
