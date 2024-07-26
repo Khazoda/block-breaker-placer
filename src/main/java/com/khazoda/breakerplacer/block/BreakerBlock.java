cffe496205e3655019e2901ffb902a4780d22ce0
Khazoda <khazoda@gmail.com>
2024-07-26 22:58:04 +0100

    placeholder block data

--- src/main/java/com/khazoda/breakerplacer/block/BreakerBlock.java
+++ src/main/java/com/khazoda/breakerplacer/block/BreakerBlock.java
@@ -1,11 +1,56 @@
 package com.khazoda.breakerplacer.block;
 
-import net.minecraft.block.AbstractBlock;
+import com.mojang.serialization.MapCodec;
 import net.minecraft.block.Block;
-import net.minecraft.item.Item;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.FacingBlock;
+import net.minecraft.item.ItemPlacementContext;
+import net.minecraft.state.StateManager;
+import net.minecraft.state.property.DirectionProperty;
+import net.minecraft.state.property.Properties;
+import net.minecraft.util.BlockMirror;
+import net.minecraft.util.BlockRotation;
+import net.minecraft.util.math.Direction;
+import org.jetbrains.annotations.Nullable;
 
-public class BreakerBlock extends Block {
+public class BreakerBlock extends FacingBlock {
+  public static final MapCodec<BreakerBlock> CODEC = createCodec(BreakerBlock::new);
+  public static final DirectionProperty FACING = Properties.FACING;
+  public static final Settings defaultSettings = Settings.create();
+
+  public BreakerBlock(Settings settings) {
+    super(settings);
+    setDefaultState(this.stateManager.getDefaultState().with(FACING, Direction.NORTH));
+  }
+
   public BreakerBlock() {
-    super(AbstractBlock.Settings.create());
+    this(defaultSettings);
+  }
+
+
+  @Override
+  protected void appendProperties(StateManager.Builder<Block, BlockState> builder) {
+    builder.add(FACING);
+  }
+
+  @Nullable
+  @Override
+  public BlockState getPlacementState(ItemPlacementContext ctx) {
+    return this.getDefaultState().with(Properties.FACING, ctx.getPlayerLookDirection().getOpposite());
+  }
+
+  @Override
+  protected BlockState rotate(BlockState state, BlockRotation rotation) {
+    return state.with(FACING, rotation.rotate(state.get(FACING)));
+  }
+
+  @Override
+  protected BlockState mirror(BlockState state, BlockMirror mirror) {
+    return state.rotate(mirror.getRotation(state.get(FACING)));
+  }
+
+  @Override
+  protected MapCodec<? extends BreakerBlock> getCodec() {
+    return CODEC;
   }
 }
--- src/main/java/com/khazoda/breakerplacer/block/PlacerBlock.java
+++ src/main/java/com/khazoda/breakerplacer/block/PlacerBlock.java
@@ -1,10 +1,53 @@
 package com.khazoda.breakerplacer.block;
 
-import net.minecraft.block.AbstractBlock;
+import com.mojang.serialization.MapCodec;
 import net.minecraft.block.Block;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.FacingBlock;
+import net.minecraft.item.ItemPlacementContext;
+import net.minecraft.state.StateManager;
+import net.minecraft.state.property.DirectionProperty;
+import net.minecraft.state.property.Properties;
+import net.minecraft.util.BlockMirror;
+import net.minecraft.util.BlockRotation;
+import org.jetbrains.annotations.Nullable;
 
-public class PlacerBlock extends Block {
+public class PlacerBlock extends FacingBlock {
+  public static final MapCodec<PlacerBlock> CODEC = createCodec(PlacerBlock::new);
+  public static final DirectionProperty FACING = Properties.FACING;
+  public static final Settings defaultSettings = Settings.create();
+
+  public PlacerBlock(Settings settings) {
+    super(settings);
+  }
+
   public PlacerBlock() {
-    super(AbstractBlock.Settings.create());
+    this(defaultSettings);
+  }
+
+  @Override
+  protected void appendProperties(StateManager.Builder<Block, BlockState> builder) {
+    builder.add(FACING);
+  }
+
+  @Nullable
+  @Override
+  public BlockState getPlacementState(ItemPlacementContext ctx) {
+    return this.getDefaultState().with(Properties.FACING, ctx.getPlayerLookDirection().getOpposite());
+  }
+
+  @Override
+  protected BlockState rotate(BlockState state, BlockRotation rotation) {
+    return state.with(FACING, rotation.rotate(state.get(FACING)));
+  }
+
+  @Override
+  protected BlockState mirror(BlockState state, BlockMirror mirror) {
+    return state.rotate(mirror.getRotation(state.get(FACING)));
+  }
+
+  @Override
+  protected MapCodec<? extends PlacerBlock> getCodec() {
+    return CODEC;
   }
 }
