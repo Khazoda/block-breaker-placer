edea0dcdafd5fdf5cbe7a4f68fc00af35fc74525
Khazoda <khazoda@gmail.com>
2024-08-08 00:46:36 +0100

    block breaking behaviour fixes

--- src/main/java/com/khazoda/breakerplacer/block/BreakerBlock.java
+++ src/main/java/com/khazoda/breakerplacer/block/BreakerBlock.java
@@ -4,20 +4,22 @@ import com.khazoda.breakerplacer.Constants;
 import com.khazoda.breakerplacer.block.entity.BreakerBlockEntity;
 import com.khazoda.breakerplacer.registry.RBlockEntity;
 import com.mojang.serialization.MapCodec;
-import net.fabricmc.fabric.api.entity.FakePlayer;
-import net.minecraft.block.Block;
-import net.minecraft.block.BlockState;
-import net.minecraft.block.Blocks;
+import net.minecraft.block.*;
 import net.minecraft.block.entity.BlockEntity;
+import net.minecraft.block.enums.DoubleBlockHalf;
+import net.minecraft.block.pattern.CachedBlockPosition;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.fluid.Fluids;
 import net.minecraft.item.*;
 import net.minecraft.loot.LootTable;
 import net.minecraft.loot.LootTables;
 import net.minecraft.loot.context.LootContextParameterSet;
 import net.minecraft.loot.context.LootContextParameters;
 import net.minecraft.loot.context.LootContextTypes;
-import net.minecraft.particle.ParticleTypes;
+import net.minecraft.registry.Registries;
 import net.minecraft.registry.RegistryKey;
+import net.minecraft.registry.tag.BlockTags;
+import net.minecraft.registry.tag.TagKey;
 import net.minecraft.screen.NamedScreenHandlerFactory;
 import net.minecraft.server.world.ServerWorld;
 import net.minecraft.state.property.Properties;
@@ -29,11 +31,14 @@ import net.minecraft.util.math.Direction;
 import net.minecraft.util.math.Vec3d;
 import net.minecraft.util.math.random.Random;
 import net.minecraft.world.World;
+import net.minecraft.world.WorldEvents;
 import net.minecraft.world.event.GameEvent;
 import org.jetbrains.annotations.Nullable;
 
 import java.util.Collections;
 import java.util.List;
+
+import static net.minecraft.block.DoorBlock.HALF;
 
 public class BreakerBlock extends TemplateBlock {
   public static final MapCodec<BreakerBlock> CODEC = createCodec(BreakerBlock::new);
@@ -58,9 +63,11 @@ public class BreakerBlock extends TemplateBlock {
     }
     return ActionResult.SUCCESS;
   }
+
   /* TODO: BREAKING BEHAVIOUR IS WEIRD, NEEDS TO BE CONSISTENT WITH THE TOOL USED */
   protected void activate(ServerWorld world, BlockState state, BlockPos pos) {
     BreakerBlockEntity be = world.getBlockEntity(pos, RBlockEntity.BREAKER_BLOCK_ENTITY).orElse(null);
+
     if (be == null) {
       Constants.LOG.warn("No matching block entity at {}, skipping block break attempt", pos);
     } else {
@@ -74,20 +81,29 @@ public class BreakerBlock extends TemplateBlock {
       BlockState targetBlockState = world.getBlockState(targetPos);
       Block targetBlock = targetBlockState.getBlock();
       BlockEntity targetBE = targetBlockState.hasBlockEntity() ? world.getBlockEntity(targetPos) : null;
-      ItemStack toolForBreaking = new ItemStack(Items.SHEARS);
-
-//      Block.dropStacks(state, world, targetPos, world.getBlockEntity(targetPos));
+      ItemStack toolForBreaking = new ItemStack(Items.STONE_PICKAXE);
       world.addBlockBreakParticles(targetPos, targetBlockState);
 
+
       try {
-        world.getPlayers().getFirst().sendMessage(Text.literal(getDroppedStacks(targetBlockState, world, targetPos, targetBE, toolForBreaking).toString() + "  |  " + toolForBreaking));
+        //world.getPlayers().getFirst().sendMessage(Text.literal(getDroppedStacks(targetBlockState, world, targetPos, targetBE, toolForBreaking).toString() + "  |  " + toolForBreaking));
+
         List<ItemStack> l = getDroppedStacks(targetBlockState, world, targetPos, targetBE, toolForBreaking);
         l.forEach(stack -> {
               // Drop items on floor if breaker is full
               if (!be.addToFirstFreeSlot(stack)) {
                 dropStacks(targetBlockState, world, targetPos, targetBE);
               }
+
+              /* Break upper part of door */
+              if (targetBlockState.getBlock() instanceof DoorBlock) {
+                breakTallBlock(world, targetPos, targetBlockState);
+              }
+
               world.setBlockState(targetPos, Blocks.AIR.getDefaultState(), Block.NOTIFY_LISTENERS | Block.FORCE_STATE);
+              world.updateNeighbors(targetPos, targetBlock);
+              state.updateNeighbors(world, targetPos, Block.NOTIFY_LISTENERS);
+              world.emitGameEvent(GameEvent.BLOCK_DESTROY, targetPos, GameEvent.Emitter.of(targetBlockState));
             }
         );
         // If no blocks were broken, do custom logic
@@ -102,21 +118,47 @@ public class BreakerBlock extends TemplateBlock {
     }
   }
 
-  /* TODO: BREAKING BEHAVIOUR IS WEIRD, NEEDS TO BE CONSISTENT WITH THE TOOL USED */
+  /* TODO: Implement sound, particles on break, and a way to swap what tool the breaker holds */
   public List<ItemStack> getDroppedStacks(BlockState state, ServerWorld world, BlockPos pos, @Nullable BlockEntity blockEntity, ItemStack tool) {
+    CachedBlockPosition cachedPos = new CachedBlockPosition(world, pos, false);
+    RegistryKey<LootTable> registryKey = state.getBlock().getLootTableKey();
+    if (registryKey == LootTables.EMPTY) return Collections.emptyList();
+
     LootContextParameterSet.Builder builder = new LootContextParameterSet.Builder(world)
         .add(LootContextParameters.ORIGIN, Vec3d.ofCenter(pos))
         .add(LootContextParameters.TOOL, tool)
+        .add(LootContextParameters.BLOCK_STATE, state)
         .addOptional(LootContextParameters.BLOCK_ENTITY, blockEntity);
-    RegistryKey<LootTable> registryKey = state.getBlock().getLootTableKey();
 
-    if (registryKey == LootTables.EMPTY) {
-      return Collections.emptyList();
+    LootContextParameterSet parameterSet = builder.build(LootContextTypes.BLOCK);
+    ServerWorld serverWorld = parameterSet.getWorld();
+    LootTable lootTable = serverWorld.getServer().getReloadableRegistries().getLootTable(registryKey);
+
+    if (state.isToolRequired()) {
+      if (tool.canBreak(cachedPos) || tool.getItem().isCorrectForDrops(tool, state)) {
+        return lootTable.generateLoot(parameterSet);
+      }
     } else {
-      LootContextParameterSet lootContextParameterSet = builder.add(LootContextParameters.BLOCK_STATE, state).build(LootContextTypes.BLOCK);
-      ServerWorld serverWorld = lootContextParameterSet.getWorld();
-      LootTable lootTable = serverWorld.getServer().getReloadableRegistries().getLootTable(registryKey);
-      return lootTable.generateLoot(lootContextParameterSet);
+      return lootTable.generateLoot(parameterSet);
+    }
+    return Collections.emptyList();
+  }
+
+  /**
+   * Implementation of breaking a double high block without a player component
+   *
+   * @see net.minecraft.block.TallPlantBlock#onBreakInCreative(World, BlockPos, BlockState, PlayerEntity)
+   */
+  protected static void breakTallBlock(World world, BlockPos pos, BlockState state) {
+    DoubleBlockHalf doubleBlockHalf = state.get(HALF);
+    if (doubleBlockHalf == DoubleBlockHalf.LOWER) {
+      BlockPos blockPos = pos.up();
+      BlockState blockState = world.getBlockState(blockPos);
+      if (blockState.isOf(state.getBlock()) && blockState.get(HALF) == DoubleBlockHalf.UPPER) {
+        BlockState blockState2 = blockState.getFluidState().isOf(Fluids.WATER) ? Blocks.WATER.getDefaultState() : Blocks.AIR.getDefaultState();
+        world.setBlockState(blockPos, blockState2, Block.NOTIFY_ALL | Block.SKIP_DROPS);
+        world.syncWorldEvent(WorldEvents.BLOCK_BROKEN, blockPos, Block.getRawIdFromState(blockState));
+      }
     }
   }
 
--- src/main/java/com/khazoda/breakerplacer/screen/BreakerScreenHandler.java
+++ src/main/java/com/khazoda/breakerplacer/screen/BreakerScreenHandler.java
@@ -60,27 +60,4 @@ public class BreakerScreenHandler extends Generic3x3ContainerScreenHandler {
   public boolean canUse(PlayerEntity player) {
     return this.inventory.canPlayerUse(player);
   }
-
-  @Override
-  public ItemStack quickMove(PlayerEntity player, int invSlot) {
-    ItemStack newStack = ItemStack.EMPTY;
-    Slot slot = this.slots.get(invSlot);
-    if (slot != null && slot.hasStack()) {
-      ItemStack originalStack = slot.getStack();
-      newStack = originalStack.copy();
-      if (invSlot < this.inventory.size()) {
-        if (!this.insertItem(originalStack, this.inventory.size(), this.slots.size(), true)) {
-          return ItemStack.EMPTY;
-        }
-      } else if (!this.insertItem(originalStack, 0, this.inventory.size(), false)) {
-        return ItemStack.EMPTY;
-      }
-      if (originalStack.isEmpty()) {
-        slot.setStack(ItemStack.EMPTY);
-      } else {
-        slot.markDirty();
-      }
-    }
-    return newStack;
-  }
 }
